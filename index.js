51097999e0ba26367b8627cfcb7cf15959dc149c
EviBera <beraevi@gmail.com>
2024-09-27 14:03:25 +0200

    Modify index.js to be able to run from terminal

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 import { createApp } from "./src/app.js";
-import { join } from "node:path";
-import { createStore } from "./src/file-store.js";
+import { join, dirname } from "node:path";
+import { createStore } from "./src/file-store.js";   //DI
 import { AppError } from "./src/app-error.js";
+import { fileURLToPath } from 'url';
 
-const STORE_PATH = join(import.meta.dirname,'todos.json');
+
+// Get the current module directory
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const STORE_PATH = join(__dirname,'todos.json');
 const todoStore = createStore(STORE_PATH);
 
 try {
