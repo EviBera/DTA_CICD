8e21ffda84e25c17f54ed24c161c6a46deac665e
kabosnn <anagynandor@gmail.com>
2024-10-02 10:48:58 +0200

    Adds a status case to list all the filtered elements

--- src/app.js
+++ src/app.js
@@ -1,25 +1,34 @@
-import { list, formatList, format, add } from './todo.js';
-import { display } from './display.js';
-import { AppError } from './app-error.js';
-import { validateAddParams } from './validate.js';
+import { list, formatList, format, add, findByStatus } from "./todo.js";
+import { display } from "./display.js";
+import { AppError } from "./app-error.js";
+import { validateAddParams, validateStatusParams } from "./validate.js";
 
 export function createApp(todoStore, args) {
   const [, , command, ...params] = args;
 
   switch (command) {
-    case 'list':
-      const todos = list(todoStore)
-      display([
-        ...formatList(todos), 
-        `You have ${todos.length} todos.`
-      ]);
+    case "list":
+      const todos = list(todoStore);
+      display([...formatList(todos), `You have ${todos.length} todos.`]);
       break;
-    case 'add':
+    case "add":
       const validated = validateAddParams(params);
       const added = add(todoStore, validated);
-      display(['New Todo added:', format(added)])
+      display(["New Todo added:", format(added)]);
+      break;
+    case "status":
+      const statusParams = validateStatusParams(params);
+      const listedByStatus = findByStatus(todoStore, statusParams);
+      if (listedByStatus.length === 0) {
+        display([`No todos are ${statusParams}.`]);
+      } else {
+        display([
+          `Todos that are ${statusParams}`,
+          ...formatList(listedByStatus),
+        ]);
+      }
       break;
     default:
-      throw new AppError(`Unknown command: ${command}`)
+      throw new AppError(`Unknown command: ${command}`);
   }
 }
