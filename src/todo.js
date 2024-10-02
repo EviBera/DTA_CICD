2e86595ae972109299f4ad859456016e6c40e931
kabosnn <anagynandor@gmail.com>
2024-10-02 10:44:24 +0200

    add findByStatus function that filters todos by their done status

--- src/todo.js
+++ src/todo.js
@@ -1,13 +1,13 @@
 export function format(todo) {
-  return `${todo.id} - [${todo.done ? '✅': ' '}] ${todo.title}`;
+  return `${todo.id} - [${todo.done ? "✅" : " "}] ${todo.title}`;
 }
 
 export function formatList(todos) {
-  return todos.map(format)
+  return todos.map(format);
 }
 
 function nextId(todos) {
-  const ids = todos.map(todo => todo.id);
+  const ids = todos.map((todo) => todo.id);
   if (ids.length === 0) {
     return 1;
   }
@@ -16,18 +16,25 @@ function nextId(todos) {
 }
 
 export function list(store) {
-  return store.get(); 
+  return store.get();
 }
 
 export function add(store, params) {
   const [title] = params;
-  const todos = store.get()
+  const todos = store.get();
   const newTodo = {
     title,
     done: false,
-    id: nextId(todos)
-  }
-  const toStore = [...todos, newTodo]
-  store.set(toStore)
+    id: nextId(todos),
+  };
+  const toStore = [...todos, newTodo];
+  store.set(toStore);
   return newTodo;
+}
+
+export function findByStatus(store, params) {
+  const todos = store.get();
+  const status = params === "done" ? true : false;
+  const filteredTodos = todos.filter((todo) => todo.done === status);
+  return filteredTodos;
 }
